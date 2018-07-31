65286294bc38486135c40070a96e851b47ca4fbd
maheshtheknight <maheshtheknight@gmail.com>
2018-07-31 16:47:29 +0530

    Initial commit

---
+++ SampleApp/SampleApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Mahesh on 31/07/18.
+//  Copyright © 2018 Mahesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello Mahesh")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
